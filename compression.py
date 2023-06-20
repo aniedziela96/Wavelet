ac4baeba5179987206cb31de7551c4bf2a7ef94c
aniedziela96 <niedziela.aleksandra@gmail.com>
2023-06-20 14:32:33 +0200

    compression

---
+++ compression.py
@@ -0,0 +1,40 @@
+import pywt
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import wave
+import sounddevice as sd
+
+samplerate, data = wavfile.read('dataset/kobietamniebije.wav')
+
+t = np.arange(len(data)) / float(samplerate)
+np.arange(len(data))
+
+data = data / max(data) # normalization 
+
+coeffs = pywt.wavedec(data, 'bior6.8', 'per')
+
+epsilon = 0.01
+k = 0
+for i in range(1, len(coeffs)):
+    for j in range(len(coeffs[i])):
+        if abs(coeffs[i][j]) < epsilon:
+            coeffs[i][j] = 0
+            k += 1
+
+print(k/len(data))
+
+compressed = pywt.waverec(coeffs, 'bior6.8', 'per')
+
+# wavfile.write('sounds/samplecD.wav', samplerate, cD)
+# wavfile.write('sounds/samplecA.wav', samplerate, cA)
+
+# new = pywt.idwt(cA, cD, "bior6.8", 'per')
+# wavfile.write('sounds/sample.wav', samplerate, new)
+
+# sd.wait()
+# sd.play(compressed, samplerate)
+# sd.wait()
+
+v = [-1, 0, 1]
+v = [1, 0]
